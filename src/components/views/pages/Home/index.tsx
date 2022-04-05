03a2369dddd07fbbafc001ec51ef4719355feded
hotta yoshikazu <hotter6163@gmail.com>
2022-04-05 03:05:15 +0000

    change Home

--- src/components/views/pages/Home/index.tsx
+++ src/components/views/pages/Home/index.tsx
@@ -1,12 +1,12 @@
 import React, { VFC } from 'react'
 
-import { FlexContainer, BasicBox } from 'components/commons/index'
+import { FlexContainer, FlexItem } from 'components/commons/index'
 
 export const Home: VFC = () => {
   return (
     <FlexContainer>
-      <BasicBox width={30}>テスト</BasicBox>
-      <BasicBox width={50}>テスト</BasicBox>
+      <FlexItem width={30} order={2}>コンテンツ1</FlexItem>
+      <FlexItem width={50} order={1}>コンテンツ2</FlexItem>
     </FlexContainer>
   )
 }
