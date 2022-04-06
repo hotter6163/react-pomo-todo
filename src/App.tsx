ea943035f15c506e26179bc15ab54d8065d14572
hotta yoshikazu <hotter6163@gmail.com>
2022-04-06 11:30:01 +0000

    HomeでTextを使用

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React, { VFC, useContext } from 'react'
 
 import { Header, Home, Main } from 'components/views/index'
 import { WindowWidthContext } from 'components/providers/index'
-import { bodySideMargin } from 'constants/index'
 
 export const App: VFC = () => {
   const { setWindowWidth } = useContext(WindowWidthContext)
   
   if (setWindowWidth) {
     window.addEventListener('resize', () => {
-      setWindowWidth(window.innerWidth - 2 * bodySideMargin)
+      setWindowWidth(window.innerWidth)
     })
   }
   
--- src/components/views/pages/Home/index.tsx
+++ src/components/views/pages/Home/index.tsx
@@ -1,12 +1,16 @@
 import React, { VFC } from 'react'
 
-import { FlexContainer, FlexItem } from 'components/commons/index'
+import { FlexContainer, FlexItem, Text } from 'components/commons/index'
 
 export const Home: VFC = () => {
   return (
     <FlexContainer>
-      <FlexItem width="30%" backgroundColor="gray">コンテンツ1</FlexItem>
-      <FlexItem width="70%" backgroundColor="lightBlue">コンテンツ2</FlexItem>
+      <FlexItem width="30%" backgroundColor="gray">
+        <Text fontSize="l" textAlign="center">コンテンツ1</Text>
+      </FlexItem>
+      <FlexItem width="70%" backgroundColor="lightBlue">
+        <Text fontSize="l" textAlign="center">コンテンツ1</Text>
+      </FlexItem>
     </FlexContainer>
   )
 }
